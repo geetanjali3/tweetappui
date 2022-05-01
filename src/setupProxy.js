7730cfe36d7423c8f86347f57251c90689206d61
geetanjali3 <prudhvigeetanjali3@gmail.com>
2022-05-01 12:44:27 +0530

    Added tweetapp ui changes

---
+++ src/setupProxy.js
@@ -0,0 +1,30 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+ 
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware('/api1', {
+      // target: 'http://localhost:8082', // API endpoint 1
+      target: 'http://54.87.57.219:8082', // API endpoint 1
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api1": "",
+      },
+      headers: {
+        Connection: "keep-alive"
+      }
+    })
+  );
+  app.use(
+    createProxyMiddleware('/api2', {
+      // target: 'http://localhost:8083', // API endpoint 2
+      target: 'http://3.239.28.255:8083',
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api2": "",
+      },
+      headers: {
+        Connection: "keep-alive"
+      }
+    })
+  );
+}
